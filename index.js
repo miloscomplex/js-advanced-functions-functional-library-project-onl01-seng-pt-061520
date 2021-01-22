670bbf1ec37c60196a97ba02fcbefcfedf40ad4b
miloscomplex <hicallmesutton@gmail.com>
2021-01-22 01:00:51 -0600

    Done.

--- index.js
+++ index.js
@@ -4,12 +4,31 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      if (typeof collection === 'object') {
+        for (let element in collection) {
+          callback(collection[element], element, collection)
+        }
+      } else {
+        collection.forEach((element, index) => {
+          callback(element, index, collection)
+        })
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newArray = []
+      if (typeof collection === 'object') {
+        for (let element in collection) {
+          newArray.push(callback(collection[element], element, collection))
+        }
+      } else {
+        collection.forEach((element, index) => {
+          newArray.push(callback(element, index, collection))
+        })
+      }
+      return newArray
     },
 
     reduce: function() {
